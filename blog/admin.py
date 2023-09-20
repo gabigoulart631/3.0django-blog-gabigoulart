25ea3bc51972c044460070fd71c14f961cf232f3
gabigoulart631 <sousagabriela631@gmail.com>
2023-09-20 09:59:39 -0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'body_text', 'pub_date')
+    list_filter = ('pub_date',)
