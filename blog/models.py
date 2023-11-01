7c3f90111de29c228df7ff58ec3e419f7312f559
gabigoulart631 <sousagabriela631@gmail.com>
2023-11-01 10:11:50 -0300

    Post com autor

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # Create your models here.
 
 class Post(models.Model):
     body_text = models.TextField('Texto Principal')
     pub_date = models.DateTimeField('Data Publicação')
-    categoria = models.CharField('Categoria', max_length=15, choices=[('noticias', 'Notícias'),('como_fazer', 'Como Fazer'),('review', 'Review'),
-    ],
-    default=None,
-    null=True
-)
+    categoria = models.CharField(
+        'Categoria', max_length=15, choices=[
+            ('noticias', 'Notícias'),('como_fazer', 'Como Fazer'),('review', 'Review'),
+        ],
+        default=None,
+        null=True
+    )
 
+
+    autor = models.ForeignKey(
+        user,
+        editable=False,
+        on_delete=models.DO_NOTHING,
+        null=True
+    )
