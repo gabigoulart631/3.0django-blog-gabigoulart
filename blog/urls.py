f2f0599cbddc0cab76f23b5b2ce004b18a296a94
gabigoulart631 <sousagabriela631@gmail.com>
2023-11-08 11:17:58 -0300

    Envio de e-mail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog.views import (index, ola, post_show, PostDetailView, get_all_posts, get_post, PostCreateView, create_post, PostListView, SobreTemplateView, PostUpdateView, PostDeleteView)
+from blog.views import (index, ola, post_show, PostDetailView, get_all_posts, get_post, PostCreateView, create_post, PostListView, SobreTemplateView, PostUpdateView, PostDeleteView, post_send,)
 
 urlpatterns = [
     path('index/', index, name="index"),
@@ -17,6 +17,8 @@ urlpatterns = [
 ),
     path('post/<int:pk>/edit', PostUpdateView.as_view(), name="post_edit"),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name="post_delete"),
+    path('post-send/<int:post_id>', post_send, name="post_send"),
+
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
@@ -18,6 +18,10 @@ from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
 
 @csrf_exempt
 def create_post(request):
@@ -154,3 +158,38 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
     def form_valid(self, form):
         messages.success(self.request, self.success_message)
         return super(PostDeleteView, self).form_valid(form)
+    
+
+
+
+
+def post_send(request, post_id):
+    post = get_object_or_404 (Post, pk-post_id)
+    post_url = reverse_lazy('post_detail', args=[post_id])
+    try:
+        email = request.POST.get('email')
+        if len(email) < 5:
+            raise ValueError('E-mail inválido')
+        
+        link = f'{request._current_scheme_host} {post_url}'
+        template = "post/post_send"
+        text_message = render_to_string(f" {template}.txt", {'post_link': link})
+        html_message = render_to_string(f" {template}.html", {'post_link': link})
+        send_mail(
+            subject="Este assunto pode te interessar!", 
+            message=text_message,
+            from_email=settings.EMAIL_HOST_USER, 
+            recipient_list=[email],
+            html_message=html_message,
+        )
+        messages.success(
+            request, 'Postagem compartilhada com sucesso.'
+        )
+        
+    except ValueError as error:
+        messages.error(request, error)
+    except:
+        messages.error(
+            request, 'Erro ao enviar a mensagem!'
+        )
+    return redirect(post_url)
